41da712e42a1b081fef78f0104b04a6568158817
ShraddhaMulekar <mulekarshraddha@gmail.com>
2026-03-21 15:11:13 +0530

    connected with mongoDB

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 import express from "express"
 import cors from "cors"
 import dotenv from "dotenv"
+import { mongoDB } from "./config/mongodb.js"
 dotenv.config()
 
 const app = express()
@@ -11,6 +12,7 @@ app.use(cors())
 
 
 
-app.listen(port, ()=>{
+app.listen(port, async()=>{
+    await mongoDB()
     console.log(`server connected on http://localhost:${port}`)
 })
