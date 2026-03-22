653d3417f0430564ac6012478d311ce049c3f38d
ShraddhaMulekar <mulekarshraddha@gmail.com>
2026-03-22 18:27:45 +0530

    Permission Middleware

--- backend/middleware/rbacMiddleware.js
+++ backend/middleware/rbacMiddleware.js
@@ -1,10 +1,32 @@
-export const rbacMiddleware = (...roles) => {
+import { rolePermissions } from "../config/rolePermissions.js";
+
+export const allowRoles = (...roles) => {
   return (req, res, next) => {
     if (!roles.includes(req.user.role)) {
       return res.status(403).json({
         message: "Access denied: role not allowed",
       });
     }
+    next();
+  };
+};
+
+export const allowPermission = (...requirePermission) => {
+  return (req, res, next) => {
+    const userRole = req.user.role;
+
+    const userPermission = rolePermissions[userRole] || [];
+
+    const hasAccess = requirePermission.every((perm) =>
+      userPermission.includes(perm),
+    );
+
+    if (!hasAccess) {
+      return res.status(403).json({
+        msg: "Access denied: insufficient permissions",
+      });
+    }
+
     next();
   };
 };
