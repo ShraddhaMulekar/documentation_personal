e47cc952ba4f773cb76eade1bf4a7bc99e45ea67
ShraddhaMulekar <mulekarshraddha@gmail.com>
2026-03-22 18:45:05 +0530

    user related permission created.

--- backend/config/permission.js
+++ backend/config/permission.js
@@ -1,6 +1,14 @@
 export const permission = {
+  // JOB
   CREATE_JOB: "create_job",
   DELETE_JOB: "delete_job",
   VIEW_JOB: "view_job",
   UPDATE_JOB: "update_job",
+
+  // USER
+  CREATE_USER: "create_user",
+  VIEW_USERS: "view_users",
+  VIEW_SINGLE_USER: "view_single_user",
+  UPDATE_USER: "update_user",
+  DELETE_USER: "delete_user",
 };
--- backend/config/rolePermissions.js
+++ backend/config/rolePermissions.js
@@ -1,18 +1,36 @@
 import { permission } from "./permission.js";
 
 export const rolePermissions = {
-    admin:[
-        permission.CREATE_JOB,
-        permission.UPDATE_JOB,
-        permission.DELETE_JOB,
-        permission.VIEW_JOB
-    ],
-    employer:[
-        permission.CREATE_JOB,
-        permission.UPDATE_JOB,
-        permission.VIEW_JOB
-    ],
-    user:[
-        permission.VIEW_JOB
-    ]
-}
+  admin: [
+    // JOB
+    permission.CREATE_JOB,
+    permission.UPDATE_JOB,
+    permission.DELETE_JOB,
+    permission.VIEW_JOB,
+
+    // USER
+    permission.CREATE_USER,
+    permission.VIEW_USERS,
+    permission.VIEW_SINGLE_USER,
+    permission.UPDATE_USER,
+    permission.DELETE_USER,
+  ],
+  employer: [
+    // JOB
+    permission.CREATE_JOB,
+    permission.UPDATE_JOB,
+    permission.VIEW_JOB,
+
+    //USER
+    permission.VIEW_SINGLE_USER,
+    permission.UPDATE_USER,
+  ],
+  user: [
+    // JOB
+    permission.VIEW_JOB,
+
+    //USER
+    permission.VIEW_SINGLE_USER,
+    permission.UPDATE_USER,
+  ],
+};
